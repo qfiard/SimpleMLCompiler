981ab50882a7fc3495898f410cf882bdb2996b85
Quentin Fiard <quentin.fiard@polytechnique.org>
2013-02-08 16:11:11 +0100

    Updated compile function to allow several passes on ast

--- src/SimpleMLCompiler.ml
+++ src/SimpleMLCompiler.ml
@@ -6,24 +6,31 @@ open Pervasives;;
 
 let file_path = Sys.argv.(1) in
 let file = open_in file_path in
-let lexbuf = Lexing.from_channel file in
-
-let rec aux_print_file f first : unit =
-    try
-        if(first)
-        then
-            print_newline();
-        print_string (input_line f);
-        aux_print_file f false
-    with eof ->
-        () in
 
 let print_file f : unit =
+    let rec aux_print_file f first : unit =
+        try
+            if(first)
+            then
+                print_newline();
+            print_string (input_line f);
+            aux_print_file f false
+        with eof ->
+            () in
+            
     seek_in f 0;
     aux_print_file f true in
-        
-let p = Parser.program Lexer.token lexbuf in
+    
+let compile file =
+    let lexbuf = Lexing.from_channel file in
+    let ast = ref (Parser.program Lexer.token lexbuf) in
+    !ast in
+    
+let p = compile file in
     printf "Input program :";
     print_file file;
     printf "\n\n";
     outputProgram p;;
+        
+
+    
